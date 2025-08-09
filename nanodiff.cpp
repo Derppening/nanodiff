5515d79d54e1bd8a96e8df1eb941cfecb70e8231
David Mak <david.18.19.21@gmail.com>
2025-08-09 11:33:51 +0800

    WIP - Read files

--- nanodiff.cpp
+++ nanodiff.cpp
@@ -1,1 +1,84 @@
-auto main() -> int {}
+#include <cstdlib>
+
+#include <expected>
+#include <filesystem>
+#include <fstream>
+#include <optional>
+#include <print>
+#include <string>
+#include <vector>
+
+struct command_line_args {
+  std::optional<std::string> expected{std::nullopt};
+  std::optional<std::string> actual{std::nullopt};
+};
+
+// Function Prototypes
+std::expected<command_line_args, std::string> parse_cmdline(const std::vector<std::string>& args);
+
+std::expected<command_line_args, std::string> parse_cmdline(const std::vector<std::string>& args) {
+  command_line_args cmd_args{};
+
+  bool parse_options = true;
+
+  for (auto it = args.begin(); it != args.end(); ++it) {
+    // "--" delimits between options and filenames
+    if (*it == "--") {
+      parse_options = false;
+      continue;
+    }
+
+    if (parse_options) {
+      // no options yet
+    } else {
+      if (!cmd_args.expected) {
+        cmd_args.expected = std::make_optional(*it);
+      } else if (!cmd_args.actual) {
+        cmd_args.actual = std::make_optional(*it);
+      } else {
+        return std::unexpected{"Too many arguments"};
+      }
+    }
+  }
+
+  // validate options
+  if (!cmd_args.expected) {
+    return std::unexpected{"Missing expected file"};
+  }
+  if (!cmd_args.actual) {
+    return std::unexpected{"Missing actual file"};
+  }
+
+  return cmd_args;
+}
+
+auto main(int argc, char** argv) -> int {
+  std::vector<std::string> args{argv, argv + argc};
+
+  auto cmd_args_or_err = parse_cmdline(args);
+  if (!cmd_args_or_err) {
+    auto err = cmd_args_or_err.error();
+    std::print("Error: {}\n", err);
+    return 1;
+  }
+
+  auto cmd_args = *cmd_args_or_err;
+
+  std::vector<std::string> expected_absent{};
+  std::vector<std::string> actual_absent{};
+
+  std::filesystem::path expected_file{*cmd_args.expected};
+  std::filesystem::path actual_file{*cmd_args.actual};
+
+  std::ifstream expected{expected_file};
+  std::ifstream actual{actual_file};
+
+  if (!expected) {
+    std::print(stderr, "Unable to open file '{}'", expected_file.string());
+    return EXIT_FAILURE;
+  }
+  if (!actual) {
+    std::print(stderr, "Unable to open file '{}'", actual_file.string());
+    return EXIT_FAILURE;
+  }
+}
