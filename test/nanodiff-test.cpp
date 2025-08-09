5e6820bffbd5756d66b5cb68d993dd99a6d4cf02
David Mak <david.18.19.21@gmail.com>
2025-08-09 15:16:35 +0800

    Add example test case using files

--- test/nanodiff-test.cpp
+++ test/nanodiff-test.cpp
@@ -1,7 +1,20 @@
+#include <filesystem>
+#include <fstream>
+
 #include <gtest/gtest.h>
 
 namespace {
+const std::filesystem::path test_res_dir{"test_resources"};
+
 TEST(SampleTest, AlwaysTrue) {
   EXPECT_TRUE(true);
+}
+
+TEST(SampleTest, ReadFile) {
+  auto path = test_res_dir / "testcase_same_output-expected.txt";
+  EXPECT_TRUE(std::filesystem::exists(path)) << "Expected file does not exist: " << path;
+
+  std::ifstream file{path};
+  EXPECT_TRUE(file.is_open()) << "Failed to open file: " << path;
 }
 }  // namespace
